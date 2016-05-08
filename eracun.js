857b13c1d5c266366d00375a54aaf47be6cbbccd
Gregor Čas <grega.cas@gmail.com>
2016-05-08 14:03:19 +0000

    Zaključek dela na II. nalogi

--- eracun.js
+++ eracun.js
@@ -47,6 +47,10 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
+  if(zahteva.session.stranka==null){
+     odgovor.redirect('/prijava');
+  }
+  else{
   pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
           Artist.Name AS izvajalec, Track.UnitPrice * " +
           razmerje_usd_eur + " AS cena, \
@@ -68,6 +72,7 @@ streznik.get('/', function(zahteva, odgovor) {
         odgovor.render('seznam', {seznamPesmi: vrstice});
       }
   })
+ }
 })
 
 // Dodajanje oz. brisanje pesmi iz košarice
@@ -233,12 +238,14 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
+    zahteva.session.stranka=polja.seznamStrank;
     odgovor.redirect('/')
   });
 })
 
 // Odjava stranke
 streznik.post('/odjava', function(zahteva, odgovor) {
+    zahteva.session.stranka=null;
     odgovor.redirect('/prijava') 
 })
 
